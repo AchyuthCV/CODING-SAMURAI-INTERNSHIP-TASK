b3ab11d32c57bd4edc1c849d64df714b2e69d73b
Achyuth C.V <achyuthcv2020@gmail.com>
2026-01-12 16:44:29 +0530

    Update PROJECT-4_numberguessing_game.py

--- TASK 2/PROJECT-4_numberguessing_game.py
+++ TASK 2/PROJECT-4_numberguessing_game.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import random
 
-# Generate random number
+
 number = random.randint(1, 100)
 attempts = 0
 
@@ -25,27 +25,28 @@ def check_guess():
             text=f"Correct! 🎉 You guessed it in {attempts} attempts."
         )
 
-# 🔴 THIS LINE IS MANDATORY
+
 window = tk.Tk()
 
 window.title("Number Guessing Game")
 window.geometry("350x250")
 
-# Heading
+
 heading = tk.Label(window, text="Guess the Number (1-100)", font=("Arial", 14))
 heading.pack(pady=10)
 
-# Entry box
+
 entry = tk.Entry(window, font=("Arial", 12))
 entry.pack(pady=5)
 
-# Button
+
 button = tk.Button(window, text="Check Guess", command=check_guess)
 button.pack(pady=10)
 
-# Result label
+
 result_label = tk.Label(window, text="", font=("Arial", 12))
 result_label.pack(pady=10)
 
-# 🔴 THIS LINE IS ALSO MANDATORY
+
 window.mainloop()
+
